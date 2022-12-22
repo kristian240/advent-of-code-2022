72921b8b699f3d86a8d3ab3413740c4919f93e48
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-12-22 07:50:19 +0100

    :tada: Day 21

--- day-21/index.ts
+++ day-21/index.ts
@@ -2,6 +2,7 @@ import data from "./data";
 
 function getValue(monkeys: Record<string, any>, monkey: any): number {
   const value = monkey.value;
+  // console.log("getValue", monkey.name, value);
 
   if (value !== null) return monkey.value;
 
@@ -22,16 +23,69 @@ function getValue(monkeys: Record<string, any>, monkey: any): number {
   throw new Error("Unknown operator: " + monkey.operator);
 }
 
+function hasMonkey(
+  name: string,
+  monkeys: Record<string, any>,
+  monkey: any
+): boolean {
+  if (monkey.name === name) return true;
+
+  if (monkey.value !== null) return false;
+
+  return (
+    hasMonkey(name, monkeys, monkeys[monkey.left]) ||
+    hasMonkey(name, monkeys, monkeys[monkey.right])
+  );
+}
+
+function getValue2(monkeys: Record<string, any>, name: string): number {
+  let humanInLeft = hasMonkey(name, monkeys, monkeys[monkeys.root.left]);
+  let nextMonkey = humanInLeft
+    ? monkeys[monkeys.root.left]
+    : monkeys[monkeys.root.right];
+
+  let score2 = getValue(
+    monkeys,
+    monkeys[humanInLeft ? monkeys.root.right : monkeys.root.left]
+  );
+  while (nextMonkey.name !== name) {
+    humanInLeft = hasMonkey(name, monkeys, monkeys[nextMonkey.left]);
+    const operator = nextMonkey.operator;
+
+    const scoreInOther = getValue(
+      monkeys,
+      monkeys[humanInLeft ? nextMonkey.right : nextMonkey.left]
+    );
+
+    nextMonkey = humanInLeft
+      ? monkeys[nextMonkey.left]
+      : monkeys[nextMonkey.right];
+
+    switch (operator) {
+      case "+":
+        score2 -= scoreInOther;
+        break;
+      case "-":
+        score2 = humanInLeft ? score2 + scoreInOther : scoreInOther - score2;
+        break;
+      case "*":
+        score2 /= scoreInOther;
+
+        break;
+      case "/":
+        score2 = humanInLeft ? score2 * scoreInOther : scoreInOther / score2;
+        break;
+    }
+  }
+
+  return score2;
+}
+
 export async function main() {
   const monkeys: Record<string, any> = {};
   data
     .split("\n")
     .map((line) => /(\w+): ((\d+)|(\w+) ([\+\-\/\*]) (\w+))/.exec(line) || [])
-    .map((l) => {
-      console.log(l);
-
-      return l;
-    })
     .map(([, name, , rawValue, left, operator, right]) => {
       const value = parseInt(rawValue);
 
@@ -47,10 +101,11 @@ export async function main() {
     });
 
   const score1 = getValue(monkeys, monkeys.root);
+  const score2 = getValue2(monkeys, "humn");
 
   console.log("Results:");
   console.log("  1st part:", score1);
-  console.log("  2nd part:");
+  console.log("  2nd part:", score2);
 }
 
 main();
